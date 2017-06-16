6015f7c35de8c5977c668067f466656df0b12c37
andrew <andreeww2@gmail.com>
2017-06-16 12:58:50 +0200

    Modyfikacja table order i user_method

--- mysql/internal_order_method.sql
+++ mysql/internal_order_method.sql
@@ -4,5 +4,6 @@ create table internal_order_method (
     internal_order_id int not null,
     method_id int not null,
     foreign key (internal_order_id) references internal_order(id),
-    foreign key (method_id) references method(id)
+    foreign key (method_id) references method(id),
+    unique (internal_order_id, method_id)
 );
--- mysql/user_method.sql
+++ mysql/user_method.sql
@@ -4,5 +4,6 @@ create table user_method (
     user_id int not null,
     method_id int not null,
     foreign key (user_id) references user(id),
-    foreign key (method_id) references method(id)
+    foreign key (method_id) references method(id),
+    unique (user_id, method_id)
 );
