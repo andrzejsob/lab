29ae03a550ffd3350288517d9cb8f544ba439748
andrew <andreeww2@gmail.com>
2017-07-27 14:42:59 +0200

    Dodanie klasy specyfikacji SingleField

---
+++ app/validation/specification/SingleField.php
@@ -0,0 +1,26 @@
+<?php
+namespace lab\validation\specification;
+
+class SingleField
+{
+    private $fieldname;
+    private $valueSpecification;
+
+    public function __construct($field, $specification)
+    {
+        $this->fieldname = $field;
+        $this->valueSpecification = $specification;
+    }
+
+    public function getValidatedField()
+    {
+        return $this->fieldname;
+    }
+
+    public function isSatisfiedBy($candidate)
+    {
+        return $this->valueSpecification->isSatisfiedBy(
+            $candidate->get($this->fieldname)
+        );
+    }
+}
