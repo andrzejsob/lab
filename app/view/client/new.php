ea18e0d565d471951af2774607026bb5ca2b0ad0
andrew <andreeww2@gmail.com>
2017-08-03 15:27:58 +0200

    Rozszerzenie formularza nowego klienta

--- app/view/client/new.php
+++ app/view/client/new.php
@@ -1,4 +1,22 @@
+<h3>Nowy klient</h3>
+<?php
+if ($errors) {
+    foreach ($errors as $key => $value) {
+        echo '<p style="color: red">'.$value.'</p>';
+    }
+}
+?>
 <form method="post">
 <input type="hidden" name="cmd" value="client-new">
+Nazwa <br>
+<input type="text" name="name"><br>
+Ulica <br>
+<input type="text" name="street"><br>
+Kod pocztowy <br>
+<input type="text" name="zip_code"><br>
+Miejscowość <br>
+<input type="text" name="city"><br>
+NIP <br>
+<input type="text" name="nip"><br>
 <input type="submit" name="submit" value="Dodaj">
 </form>
