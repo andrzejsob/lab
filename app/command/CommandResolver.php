207c825d2b27ddf606beac5a8d2c163a7d58dbc9
andrew <andreeww2@gmail.com>
2017-10-11 11:21:12 +0200

    Dla zalogowanego uzytkownika dodano przekierowanie do cmd=client po wybraniu adresu cmd=login. Modyfikacja sprawdzenia uprawnien w commandResolver

--- app/command/CommandResolver.php
+++ app/command/CommandResolver.php
@@ -1,6 +1,9 @@
 <?php
 namespace lab\command;
 
+use lab\base\Redirect;
+use lab\domain\Permission;
+
 class CommandResolver
 {
     private static $defaultCmd;
@@ -13,15 +16,22 @@ class CommandResolver
     {
         $cmd = $request->getProperty('cmd');
         $session = \lab\base\ApplicationHelper::getSession();
-        if (!$session->getLoggedIn() && $cmd != 'login') {
-            header('Location: ?cmd=login');
+        
+        if (!$session->getLoggedIn()) {
+            if ($cmd == 'login') {
+                return array('\\lab\\command\\LoginCommand', 'indexAction');
+            }
+            new Redirect('?cmd=login');
+        } elseif ($cmd == 'login' || $cmd == 'login-index') {
+            new Redirect('?cmd=client');
         }
 
         if (!is_null($cmd)) {
             $this->setCommandFullName($cmd);
-            if ($this->commandIsCorrect()) {
-                if ($this->command != '\\lab\\command\\LoginCommand' &&
-                    !isset($session->getUser()->getPermissionsArray()[$cmd])) {
+            if ($this->isCommandCorrect()) {
+                $permArray = Permission::getFinder()->findAll()->getArray('name');
+                $userPermArray = $session->getUser()->getPermissionsArray();
+                if (in_array($cmd, $permArray) && !isset($userPermArray[$cmd])) {
                     return array($this->defaultCommand, 'permissionErrorAction');
                 }
                 return array($this->command, $this->action);
@@ -41,12 +51,11 @@ class CommandResolver
             $command = $cmd;
             $action = 'index';
         }
-
         $this->command = '\\lab\\command\\'.ucfirst($command).'Command';
         $this->action = $action.'Action';
     }
 
-    private function commandIsCorrect() {
+    private function isCommandCorrect() {
         if(class_exists($this->command) &&
             is_subclass_of($this->command, '\\lab\\command\\Command') &&
             method_exists($this->command, $this->action)) {
